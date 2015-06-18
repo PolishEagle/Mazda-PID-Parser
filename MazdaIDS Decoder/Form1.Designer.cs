9bef5400b7482747c82fdb0da643f13ca8753ca5
Jan <jan.wichanski@gmail.com>
2015-06-17 23:47:42 -0400

    Parser for MazdaIDS ddl log files to convert them into .csv files

---
+++ MazdaIDS Decoder/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MazdaIDS_Decoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.lstLogFiles = new System.Windows.Forms.CheckedListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabCtrl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chkPids = new System.Windows.Forms.ListBox();
+            this.statusBar.SuspendLayout();
+            this.tabCtrl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstLogFiles
+            // 
+            this.lstLogFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLogFiles.Location = new System.Drawing.Point(3, 37);
+            this.lstLogFiles.Name = "lstLogFiles";
+            this.lstLogFiles.Size = new System.Drawing.Size(895, 304);
+            this.lstLogFiles.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnClear.Location = new System.Drawing.Point(311, 353);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear Selection";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnConvert.Location = new System.Drawing.Point(491, 353);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(95, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to CSV";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Available Files:";
+            // 
+            // statusBar
+            // 
+            this.statusBar.GripMargin = new System.Windows.Forms.Padding(0);
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusBar.Location = new System.Drawing.Point(0, 418);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(920, 22);
+            this.statusBar.Stretch = false;
+            this.statusBar.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tabCtrl
+            // 
+            this.tabCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabCtrl.Controls.Add(this.tabPage1);
+            this.tabCtrl.Controls.Add(this.tabPage2);
+            this.tabCtrl.Location = new System.Drawing.Point(7, 0);
+            this.tabCtrl.Name = "tabCtrl";
+            this.tabCtrl.SelectedIndex = 0;
+            this.tabCtrl.Size = new System.Drawing.Size(906, 416);
+            this.tabCtrl.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lstLogFiles);
+            this.tabPage1.Controls.Add(this.btnConvert);
+            this.tabPage1.Controls.Add(this.btnClear);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(898, 390);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DDL Files";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tabPage2.Controls.Add(this.chkPids);
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(898, 390);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Charts";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.IsDockedInsideChartArea = false;
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend1.MaximumAutoSize = 10F;
+            legend1.Name = "Legend1";
+            legend1.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Tall;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 25);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(886, 359);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MazdaChart_MouseDown);
+            // 
+            // chkPids
+            // 
+            this.chkPids.FormattingEnabled = true;
+            this.chkPids.Location = new System.Drawing.Point(734, 4);
+            this.chkPids.Name = "chkPids";
+            this.chkPids.Size = new System.Drawing.Size(158, 17);
+            this.chkPids.TabIndex = 2;
+            this.chkPids.SelectedIndexChanged += new System.EventHandler(this.chkPids_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 440);
+            this.Controls.Add(this.tabCtrl);
+            this.Controls.Add(this.statusBar);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mazda IDS Log Parse - By Jan Wichanski";
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.tabCtrl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox lstLogFiles;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.TabControl tabCtrl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox chkPids;
+    }
+}
+
---
+++ MazdaIDS Decoder/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MazdaIDS_Decoder
+{
+    public partial class Form1 : Form
+    {
+        private MazdaIDS_DDL_Parser _mazdaLogParser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _mazdaLogParser = new MazdaIDS_DDL_Parser();
+
+            Task t = Task.Run(() =>
+            {
+                SetStatusBarText("Reading log files from directory...");
+
+                // Get the files and details
+                int logs = _mazdaLogParser.ReadAvailableMazdaFiles();
+
+                SetStatusBarText(string.Format("{0} log(s) found!", logs));
+            });
+            t.Wait();
+
+            // Display the files to the checkbox
+            foreach (FileInfo fileDetails in _mazdaLogParser.Logs)
+            {
+                lstLogFiles.Items.Add(string.Format("{0}  - ({1})", fileDetails.Name, fileDetails.CreationTime.ToString()));
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (int i in lstLogFiles.CheckedIndices)
+            {
+                lstLogFiles.SetItemCheckState(i, CheckState.Unchecked);
+            }
+
+            lblStatus.Text = "All items unchecked!";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (lstLogFiles.CheckedIndices.Count == 0)
+            {
+                lblStatus.Text = "Error: No items selected to convert!";
+                return;
+            }
+
+            // Get selected index before we go to a separate thread
+            List<int> indices = new List<int>();
+            indices.AddRange(lstLogFiles.SelectedIndices.Cast<int>());
+
+            Task convert = Task.Run(() =>
+            {
+                SetEssentialControlsEnable(false);
+                SetStatusBarText("Conversion: running...");
+
+                _mazdaLogParser.ConvertFileToCSV(indices);
+                
+                SetStatusBarText(lblStatus.Text = "Conversion: Completed!");
+                SetEssentialControlsEnable(true);
+
+                PlotDataOnChart(string.Empty);
+            });
+        }
+
+        private void SetStatusBarText(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action<string>)SetStatusBarText, text);
+                return;
+            }
+            lblStatus.Text = text;
+        }
+
+        private void SetEssentialControlsEnable(bool enabled)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action<bool>)SetEssentialControlsEnable, enabled);
+                return;
+            }
+            btnConvert.Enabled = enabled;
+            btnClear.Enabled = enabled;
+            lstLogFiles.Enabled = enabled;
+        }
+
+        private void PlotDataOnChart(string noneuse)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action<string>)PlotDataOnChart, string.Empty);
+                return;
+            }
+
+            // Plot the data
+            if (_mazdaLogParser.ParsedData.Count != 0)
+            {
+                // TODO: Make this work with any log file, not just 2
+                var dic = _mazdaLogParser.ParsedData;
+                var data = dic[_mazdaLogParser.Logs[1]];
+
+                foreach (var pid in data)
+                {
+                    if (chart1.Series.Where<Series>(s => s.Name.Equals(pid.PidName)).Count() == 0)
+                    {
+                        chart1.Series.Add(pid.PidName);
+
+                        foreach (var entry in pid.DataEntries)
+                        {
+                            var convertedValue = (double)pid.GetConvertedValue(entry.Value);
+
+                            chart1.Series[pid.PidName].ToolTip = "#VALY, #VALX";
+                            chart1.Series[pid.PidName].Points.Add(new DataPoint(entry.Time, convertedValue));
+                        }
+                    }
+
+                    chart1.Series[pid.PidName].Enabled = false;
+
+                    // The checkbox for enabling and disabling series
+                    if (!chkPids.Items.Contains(pid.PidName))
+                    {
+                        chkPids.Items.Add(pid.PidName);
+                    }
+                }
+            }
+        }
+
+        private void MazdaChart_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            HitTestResult result = chart1.HitTest(e.X, e.Y);
+            if (result != null && result.Series != null)
+            {
+                result.Series.Enabled = !result.Series.Enabled;
+                chart1.Update();
+            }
+        }
+
+        private void chkPids_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sender is ListBox)
+            {
+                ListBox chkLst = sender as ListBox;
+                string name = chkLst.SelectedItem.ToString();
+
+                chart1.Series[name].Enabled = !chart1.Series[name].Enabled;
+                chart1.Update();
+                chart1.ChartAreas["ChartArea1"].RecalculateAxesScale();
+            }
+        }
+    }
+}
